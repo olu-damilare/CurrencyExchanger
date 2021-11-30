6f53f12a9d918ae4b3618699fc0cc1d1a7197708
Damilare Jolayemi <djolayemi@gmail.com>
2021-11-30 23:16:58 +0100

    exchange response dto

---
+++ src/main/java/com/exchanger/currencyexchanger/model/ExchangeResponseDto.java
@@ -0,0 +1,13 @@
+package com.exchanger.currencyexchanger.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ExchangeResponseDto {
+
+    private Double rate;
+}
