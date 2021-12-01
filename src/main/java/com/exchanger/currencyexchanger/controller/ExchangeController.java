66baae696b958dbc6140fc98f362dc0db316a4b3
Damilare Jolayemi <djolayemi@gmail.com>
2021-12-01 05:51:12 +0100

    updated request method

--- src/main/java/com/exchanger/currencyexchanger/controller/ExchangeController.java
+++ src/main/java/com/exchanger/currencyexchanger/controller/ExchangeController.java
@@ -7,7 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +18,7 @@ public class ExchangeController {
     @Autowired
     ExchangeService exchangeService;
 
-    @GetMapping("/exchange-rate/")
+    @PostMapping("/exchange-rate/")
     public ResponseEntity<?> exchange(@RequestBody ExchangeRequestDto requestDto){
         log.info("request dto in controller --> {}", requestDto);
         try{
